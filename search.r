d72fc75013077c47e76f5161d4d103c283439154
Bc. Martin Kubovčík <74611856+markub3327@users.noreply.github.com>
2021-10-07 18:15:08 +0200

    Update search.r

--- search.r
+++ search.r
@@ -1,9 +1,11 @@
 library("igraph")
 
 do_sirky <- function(graph, root) {
-    D <- c(V(g)[root])
+    D <- c(as_ids(V(g)[root]))
+    mojLayout <- layout_nicely(graph, dim=2)
+    plot(graph, layout=mojLayout)
 
-    V(g)$zafarbeny <- rep(FALSE, vcount(g))
+    V(g)$zafarbeny <- FALSE
 
     while (length(D)) {
         if (V(g)[D[1]]$zafarbeny) {
@@ -18,15 +20,20 @@ do_sirky <- function(graph, root) {
             # vymaz pouzity vrchol a uloz susedov
             susedia <- neighbors(graph, D[1])
             D <- D[-1]
-            D <- c(D, susedia)
+            D <- append(D, as_ids(susedia), after = length(D))
+            plot(g, vertex.color=V(g)$zafarbeny, layout=mojLayout, main=paste("Vrcholy", D, sep=" "))
+
+            invisible(readline(prompt="Press [enter] to continue"))
         }
     }
 }
 
 do_hlbky <- function(graph, root) {
-    D <- c(V(g)[root])
+    D <- c(as_ids(V(g)[root]))
+    mojLayout <- layout_nicely(graph, dim=2)
+    plot(graph, layout=mojLayout)
 
-    V(g)$zafarbeny <- rep(FALSE, vcount(g))
+    V(g)$zafarbeny <- FALSE
 
     while (length(D)) {
         if (V(g)[D[1]]$zafarbeny) {
@@ -41,7 +48,10 @@ do_hlbky <- function(graph, root) {
             # vymaz pouzity vrchol a uloz susedov
             susedia <- neighbors(graph, D[1])
             D <- D[-1]
-            D <- append(D, susedia, after = 0)
+            D <- append(D, as_ids(susedia), after = 0)
+            plot(g, vertex.color=V(g)$zafarbeny, layout=mojLayout, main=paste("Vrcholy", D, sep=" "))
+
+            invisible(readline(prompt="Press [enter] to continue"))
         }
     }
 }
